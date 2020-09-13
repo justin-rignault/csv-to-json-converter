d09589f4b8a97ad3119f61289ff9b4bb4ee05442
justin-rignault <justinrignault@gmail.com>
2020-09-13 18:28:25 -0300

    Updates

---
+++ precache-manifest.1b53346dc3bad217a49c8d2ad0a1d394.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b097d89649cf1f0a09b500864314714e",
+    "url": "/csv-to-json-converter/index.html"
+  },
+  {
+    "revision": "ec086e4078cfa385bfdb",
+    "url": "/csv-to-json-converter/static/css/main.9161f135.chunk.css"
+  },
+  {
+    "revision": "0bfae3bfa82c2e1f2fb8",
+    "url": "/csv-to-json-converter/static/js/2.01d6cf47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/csv-to-json-converter/static/js/2.01d6cf47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec086e4078cfa385bfdb",
+    "url": "/csv-to-json-converter/static/js/main.f7d4e33d.chunk.js"
+  },
+  {
+    "revision": "f60e136210a8e800ff66",
+    "url": "/csv-to-json-converter/static/js/runtime-main.b7265903.js"
+  }
+]);
